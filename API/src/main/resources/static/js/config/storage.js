693128744994d45ab47b084604ee733b63dde123
Min <als904204@naver.com>
2023-11-29 01:53:07 +0900

    when you push the cancel btn, reload

--- API/src/main/resources/static/js/config/storage.js
+++ API/src/main/resources/static/js/config/storage.js
@@ -9,10 +9,13 @@ function onFileSelect() {
   }
 }
 
+// 취소 버튼 누를 시 localStorage 삭제
 function onCancel() {
   localStorage.removeItem('fileUploaded');
   localStorage.removeItem('uploadedCompany');
   changeToSelectButton();
+  alert('파일 업로드 취소')
+  location.reload()
 }
 
 // 로컬에 fileUploaded 가 true 일때만 그려라 (없으면 DB 엔 없는데 로컬스토리지에 남아있는거)
@@ -21,6 +24,7 @@ function checkUploadStatus() {
 
     const uploadedCompany = localStorage.getItem('uploadedCompany');
 
+    // 파일이 업로드 된 상태라면 그래프를 그림
     if (uploadedCompany) {
       changeToCancelButton();
       fetchStockDataAndRenderLineChart(uploadedCompany);
@@ -29,11 +33,12 @@ function checkUploadStatus() {
       fetchStockDateRangeData(uploadedCompany);
       fetchRecentStockData(uploadedCompany);
     }
-  } else {
+  } else { // 업로드 X 라면 업로드 버튼 활성화
     changeToSelectButton();
   }
 }
 
+// 파일 업로드 버튼 활성화
 function changeToSelectButton() {
   document.getElementById('selectButton').style.display = 'inline-block';
   document.getElementById('uploadButton').style.display = 'none';
@@ -41,6 +46,7 @@ function changeToSelectButton() {
   document.getElementById('fileInput').value = ''; // 파일 입력 필드 초기화
 }
 
+// 파일 취소 버튼 활성화
 function changeToCancelButton() {
   document.getElementById('cancelButton').style.display = 'inline-block';
   document.getElementById('selectButton').style.display = 'none';
